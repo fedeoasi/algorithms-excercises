54478232c4926f71c72faf9a75e551f08ca1acb5
fcaimi <fedeoasi@gmail.com>
2013-11-30 20:08:05 -0600

    Add ComparisonCountingSort and test.

---
+++ src/main/java/com/github/fedeoasi/ComparisonCountingSort.java
@@ -0,0 +1,21 @@
+package com.github.fedeoasi;
+
+public class ComparisonCountingSort {
+    public static int[] sort(int[] a) {
+        int[] s = new int[a.length];
+        int[] count = new int[a.length];
+        for(int i = 0; i < a.length - 1; i++) {
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[i] < a[j]) {
+                    count[j]++;
+                } else {
+                    count[i]++;
+                }
+            }
+        }
+        for(int i = 0; i < s.length; i++) {
+            s[count[i]] = a[i];
+        }
+        return s;
+    }
+}
---
+++ src/test/scala/com/github/fedeoasi/CountingSortTest.scala
@@ -0,0 +1,27 @@
+package com.github.fedeoasi
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class CountingSortTest extends FunSpec with ShouldMatchers {
+  import ComparisonCountingSort._
+
+  it("should sort an empty array") {
+    sort(Array()) should be(Array())
+  }
+
+  it("should sort a single element") {
+    val array = Array(1)
+    sort(array) should be(array)
+  }
+
+  it("should sort a simple array") {
+    val array = Array(60, 35, 81, 98, 14, 47)
+    sort(array) should be(Array(14, 35, 47, 60, 81, 98))
+  }
+
+  it("should sort an array with duplicates") {
+    val array = Array(60, 35, 81, 81, 14, 47)
+    sort(array) should be(Array(14, 35, 47, 60, 81, 81))
+  }
+}

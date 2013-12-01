62598aac2bff7f088070a15b2d51189d5bd4dd46
fcaimi <fedeoasi@gmail.com>
2013-11-30 21:35:55 -0600

    Add iterative fibonacci and test.

---
+++ src/main/java/com/github/fedeoasi/Fibonacci.java
@@ -0,0 +1,15 @@
+package com.github.fedeoasi;
+
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        int first = 1;
+        int second = 1;
+        int result = first;
+        for(int i = 3; i <= n; i++) {
+            result = first + second;
+            first = second;
+            second = result;
+        }
+        return result;
+    }
+}
---
+++ src/test/scala/com/github/fedeoasi/FibonacciTest.scala
@@ -0,0 +1,28 @@
+package com.github.fedeoasi
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+
+class FibonacciTest extends FunSpec with ShouldMatchers {
+  import Fibonacci._
+
+  it("should compute the first number of the sequence") {
+    fibonacci(1) should be(1)
+  }
+
+  it("should compute the second number of the sequence") {
+    fibonacci(2) should be(1)
+  }
+
+  it("should compute the third number of the sequence") {
+    fibonacci(3) should be(2)
+  }
+
+  it("should compute the fourth number of the sequence") {
+    fibonacci(4) should be(3)
+  }
+
+  it("should compute a higher number of the sequence") {
+    fibonacci(36) should be(14930352)
+  }
+}

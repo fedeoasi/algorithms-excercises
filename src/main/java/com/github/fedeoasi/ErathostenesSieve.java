3258eba41c71a77759ffb7b737300c54a3d81892
fcaimi <fedeoasi@gmail.com>
2013-11-30 19:34:53 -0600

    Implement ErathostenesSieve in java and test.

---
+++ src/main/java/com/github/fedeoasi/ErathostenesSieve.java
@@ -0,0 +1,31 @@
+package com.github.fedeoasi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ErathostenesSieve {
+    public static List<Integer> primeNumbersUpTo(int n) {
+        if(n < 2) return new ArrayList<>();
+        int[] cand = new int[n - 1];
+        for(int i = 2; i < n; i++) {
+            cand[i - 2] = i;
+        }
+        double sqrt = Math.sqrt(n);
+        for(int i = 2; i <= (int) sqrt; i++) {
+            if(cand[i - 2] != 0) {
+                for(int j = i * i; j <= n; j++) {
+                    if(j % i == 0) {
+                        cand[j - 2] = 0;
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < n - 2; i++) {
+            if(cand[i] != 0) {
+                result.add(cand[i]);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/scala/com/github/fedeoasi/SieveTest.scala
@@ -0,0 +1,41 @@
+package com.github.fedeoasi
+
+import org.scalatest.FunSpec
+import org.scalatest.matchers.ShouldMatchers
+import scala.collection.JavaConverters
+import java.util
+
+class SieveTest extends FunSpec with ShouldMatchers {
+  import ErathostenesSieve._
+
+  it("should return no prime numbers when passed 0") {
+    val res = primeNumbersUpTo(0)
+    res.toArray should be(Array())
+  }
+
+  it("should return no prime numbers when passed 1") {
+    primeNumbersUpTo(1).toArray should be(Array())
+  }
+
+  it("should return the prime numbers up to a non prime") {
+    primeNumbersUpTo(10).toArray should be(Array(2, 3, 5, 7))
+  }
+
+  it("should return the prime numbers up to a prime number") {
+    primeNumbersUpTo(11).toArray should be(Array(2, 3, 5, 7))
+  }
+
+  it("should return the first primes up to 100") {
+    primeNumbersUpTo(100).toArray should be(
+      Array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+          43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
+    )
+  }
+
+  it("times the execution for the first million primes") {
+    val start = System.currentTimeMillis()
+    primeNumbersUpTo(1000000)
+    val stop = System.currentTimeMillis()
+    println(s"primeNumbersUpTo(1000000) elapsed: ${(stop - start) / 1000} seconds")
+  }
+}

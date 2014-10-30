a17a1be0885e1018327ffb39a8368f45fa59d7c6
fcaimi <fedeoasi@gmail.com>
2014-10-29 19:50:21 -0500

    Shorten spiral order code a bit.

--- src/main/java/com/github/fedeoasi/arrays/MatrixSpiralOrder.java
+++ src/main/java/com/github/fedeoasi/arrays/MatrixSpiralOrder.java
@@ -11,45 +11,31 @@ public class MatrixSpiralOrder {
         int[] result = new int[resultSize];
 
         Position p = new Position(0, 0);
-        int rightTarget = h - 1;
-        int downTarget = h - 1;
-        int leftTarget = 0;
-        int upTarget = 1;
+        int offset = 1;
         for(int i = 0; i < resultSize; ) {
-            result[i++] = get(matrix, p);
-
-            p.right();
-            while (p.y <= rightTarget) {
+            //Center of the matrix
+            if(i == resultSize - 1) {
+                result[i++] = get(matrix, p);
+            }
+            while (p.y < w - offset) {
                 result[i++] = get(matrix, p);
                 p.right();
             }
-            p.left();
-            rightTarget -= 1;
-
-            p.down();
-            while (p.x <= downTarget) {
+            while (p.x < w - offset) {
                 result[i++] = get(matrix, p);
                 p.down();
             }
-            p.up();
-            downTarget -= 1;
-
-            p.left();
-            while (p.y >= leftTarget) {
+            while (p.y > offset - 1) {
                 result[i++] = get(matrix, p);
                 p.left();
             }
-            p.right();
-            leftTarget += 1;
-
-            p.up();
-            while (p.x >= upTarget) {
+            while (p.x > offset - 1) {
                 result[i++] = get(matrix, p);
                 p.up();
             }
             p.down();
-            upTarget += 1;
             p.right();
+            offset += 1;
         }
         return result;
     }

3e79ed2ac505f7ae5682b4dbf654d98c928c70ec
fcaimi <fedeoasi@gmail.com>
2014-11-30 09:26:18 -0600

    Template and helper methods for the hackerrank problems.

---
+++ src/main/scala/com/github/fedeoasi/hackerrank/HackerRankTemplate.scala
@@ -0,0 +1,26 @@
+package com.github.fedeoasi.hackerrank
+
+object HackerRankTemplate {
+  object Solution {
+    def computeSolution(): Int = ???
+
+    def main(args: Array[String]) {
+      val lines = io.Source.stdin.getLines()
+      val T = lines.take(1).next().toInt
+      1 to T foreach { i =>
+        println(computeSolution())
+      }
+    }
+
+    def readArray(input: Iterator[String]): Array[Int] = {
+      input.take(1).next().split(" ").map(_.toInt).toArray
+    }
+
+    def readInt(input: Iterator[String]): Int = input.take(1).next().toInt
+
+    def readTwoInts(input: Iterator[String]): (Int, Int) = {
+      val numbers = input.take(1).next().split(" ").map(_.toInt).toArray
+      (numbers(0), numbers(1))
+    }
+  }
+}

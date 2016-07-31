64a76610f842c533e99cbb8be88d2f115ddd1eea
fcaimi <fedeoasi@gmail.com>
2016-07-30 22:10:19 -0500

    Correct but slow solution.

--- src/main/scala/com/github/fedeoasi/intervals/IntervalStrength.scala
+++ src/main/scala/com/github/fedeoasi/intervals/IntervalStrength.scala
@@ -1,24 +1,96 @@
 package com.github.fedeoasi.intervals
 
-case class Interval(a: Int, b: Int) {
-  def intersects(other: Interval): Boolean = {
-    other.a >= a && other.a <= b ||
-      other.b >= a && other.b <= b
+import java.io.{BufferedReader, FileReader}
+import java.util
+import java.util.Date
+import java.util.Map.Entry
+
+import scala.collection.JavaConverters._
+
+case class Interval(id: Int, a: Int, b: Int) extends Ordered[Interval] {
+  def intersects(that: Interval): Boolean = {
+    that.a >= a && that.a <= b ||
+      that.b >= a && that.b <= b
+  }
+
+  import scala.math.Ordered.orderingToOrdered
+
+  override def compare(that: Interval): Int = {
+    (this.a, this.b).compare((that.a, that.b))
   }
 }
 
 class IntervalStrength(source: Seq[Interval], target: Seq[Interval]) {
   def computeSolution: Int = {
+    val targetByA = target.groupBy(_.a)
+    val targetByB = target.groupBy(_.b)
+
+    val allKeys = (targetByA.keys ++ targetByB.keys ++ targetByB.keys.map(_ + 1)).toSeq.sorted
+
+    println(s"${new Date()} - preprocessing")
+
+    val intervalIdsByPosition: util.TreeMap[Int, Set[Int]] = {
+      val _map = new util.TreeMap[Int, Set[Int]]()
+      allKeys.foldLeft((Set.empty[Int], Set.empty[Int])) { case ((currentSet, toBeRemoved), key) =>
+        var newSet = currentSet
+        targetByA.get(key) match {
+          case Some(startingIntervals) =>
+            startingIntervals.foreach { i => newSet = newSet + i.id }
+          case None =>
+        }
+        val newToBeRemoved = targetByB.get(key) match {
+          case Some(endingIntervals) => endingIntervals.map(_.id).toSet
+          case None => Set.empty[Int]
+        }
+        newSet = newSet -- toBeRemoved
+        _map.put(key, newSet)
+        (newSet, newToBeRemoved)
+      }
+      _map
+    }
+
+    println(s"${new Date()} - Computing solution")
     source.map { s =>
-      strengthFor(s)
+      val optionalLowerBound = Option(intervalIdsByPosition.floorEntry(s.a)).map(_.getKey)
+      val optionalUpperBound = Option(intervalIdsByPosition.floorEntry(s.b)).map(_.getKey)
+
+      optionalUpperBound match {
+        case None => 0
+        case Some(upperBound) =>
+          val lowerBound = optionalLowerBound.getOrElse(intervalIdsByPosition.firstKey())
+          val subMap = intervalIdsByPosition.subMap(lowerBound, true, upperBound, true)
+          subMap.asScala.foldLeft(Set.empty[Int]) {
+            case (acc, (_, set)) =>
+              acc ++ set
+          }.size
+      }
     }.sum
   }
 
   def strengthFor(s: Interval): Int = {
     target.map { t =>
-      println(s"$s $t ${s.intersects(t)}")
       if (s.intersects(t)) 1 else 0
     }.sum
   }
 }
 
+object Solution {
+  def main(args: Array[String]) {
+    val inputFile = "/home/fcaimi/Documents/hackerrank/x-and-his-shots/input03.txt"
+//    solve(new BufferedReader(new InputStreamReader(System.in)))
+    solve(new BufferedReader(new FileReader(inputFile)))
+  }
+
+  def solve(input: BufferedReader): Unit = {
+    val Array(n, m) = input.readLine().split(" ").map(_.toInt)
+    val target = Range(0, n).map { i =>
+      val Array(a, b) = input.readLine().split(" ").map(_.toInt)
+      Interval(i + 1, a, b)
+    }
+    val source = Range(0, m).map { i =>
+      val Array(a, b) = input.readLine().split(" ").map(_.toInt)
+      Interval(i + 1, a, b)
+    }
+    println(new IntervalStrength(source, target).computeSolution)
+  }
+}
--- src/test/scala/com/github/fedeoasi/intervals/IntervalIntersectSpec.scala
+++ src/test/scala/com/github/fedeoasi/intervals/IntervalIntersectSpec.scala
@@ -3,30 +3,57 @@ package com.github.fedeoasi.intervals
 import org.scalatest.{FunSpec, Matchers}
 
 class IntervalIntersectSpec extends FunSpec with Matchers {
-  it("two non intersecting intervals") {
-    val source = Seq(Interval(3, 5))
-    val target = Seq(Interval(1, 2))
+  it("single target does not intersect single source") {
+    val source = Seq(Interval(1, 3, 5))
+    val target = Seq(Interval(2, 1, 2))
     new IntervalStrength(target, source).computeSolution shouldBe 0
   }
 
-  it("two intersecting intervals") {
-    val source = Seq(Interval(1, 5))
-    val target = Seq(Interval(1, 2))
+  it("single target intersects single source") {
+    val source = Seq(Interval(1, 1, 5))
+    val target = Seq(Interval(2, 1, 2))
     new IntervalStrength(target, source).computeSolution shouldBe 1
   }
 
+  describe("source has two intersecting intervals") {
+    val source = Seq(Interval(1, 1, 5), Interval(2, 3, 7))
+
+    it("target does not intersect with any") {
+      new IntervalStrength(Seq(Interval(10, 0, 0)), source).computeSolution shouldBe 0
+      new IntervalStrength(Seq(Interval(10, 8, 9)), source).computeSolution shouldBe 0
+    }
+
+    it("target is a subset of the second one") {
+      new IntervalStrength(Seq(Interval(2, 6, 7)), source).computeSolution shouldBe 1
+    }
+
+    it("target is the same as the first one") {
+      new IntervalStrength(Seq(Interval(2, 1, 5)), source).computeSolution shouldBe 2
+    }
+
+    it("target is both of them combined") {
+      new IntervalStrength(Seq(Interval(2, 1, 7)), source).computeSolution shouldBe 2
+    }
+
+    it("target is the same as the second one") {
+      new IntervalStrength(Seq(Interval(2, 3, 7)), source).computeSolution shouldBe 2
+    }
+  }
+
+
+
   it("computes the full solution") {
     val source = Seq(
-      Interval(1, 5),
-      Interval(2, 3),
-      Interval(4, 7),
-      Interval(5, 7)
+      Interval(1, 1, 5),
+      Interval(2, 2, 3),
+      Interval(3, 4, 7),
+      Interval(4, 5, 7)
     )
     val target = Seq(
-      Interval(1, 2),
-      Interval(2, 3),
-      Interval(4, 5),
-      Interval(6, 7)
+      Interval(1, 1, 2),
+      Interval(2, 2, 3),
+      Interval(3, 4, 5),
+      Interval(4, 6, 7)
     )
     new IntervalStrength(source, target).computeSolution shouldBe 9
   }
